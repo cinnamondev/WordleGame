f0ff592ef92379c648cc9ab8caf5f7171b0f0ae1
cinnamondev <justpromptiguess@gmail.com>
2022-09-18 16:38:03 +0100

    adapted to always show positioninfo (suppose it could be done better...)

--- src/Wordle.rs
+++ src/Wordle.rs
@@ -15,31 +15,45 @@ impl ToString for PositionInfo {
             PositionInfo::Correct(c) => format!("\x1b[0;30;42m {} \x1b[0m ",c),
             PositionInfo::Placement(c) => format!("\x1b[0;30;43m {} \x1b[0m ",c),
             PositionInfo::Incorrect(c) => format!("\x1b[0;30;41m {} \x1b[0m ",c),
-            Unknown => "idklol".to_string()
+            _ => format!(""),
         }
     }
 }
 
 #[derive(Debug)]
 pub enum GameResult {
-    Win(u8),
-    Lose,
+    Win(u8, Vec<PositionInfo>),
+    Lose(Vec<PositionInfo>),
     Guess(Vec<PositionInfo>)
 }
 
+impl GameResult {
+    pub fn parse_guess(guess: &Vec<PositionInfo>) -> String {
+        guess
+            .iter()
+            .map(|i| i.to_string())
+            .collect()
+    }
+}
 impl fmt::Display for GameResult {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            GameResult::Win(guesses) => write!(f,"You win! Won in {} guesses.",guesses),
-            GameResult::Lose => write!(f,"You lose! :("),
-            GameResult::Guess(pos) => write!(f,"{}",
-                                             pos
-                                                 .iter()
-                                                 .map(|i| i.to_string())
-                                                 .collect::<String>())
+            GameResult::Win(guesses,pos) => {
+                let blocks = GameResult::parse_guess(pos);
+                write!(f,"You win! Won in {} guesses. Guess: {}",guesses,blocks)
+            },
+            GameResult::Lose(pos) => {
+                let blocks = GameResult::parse_guess(pos);
+                write!(f,"You lose! :(")
+            },
+            GameResult::Guess(pos) => {
+                let blocks = GameResult::parse_guess(pos);
+                write!(f,"{}", blocks)
+            }
         }
     }
 }
+
 
 #[derive(Debug)]
 pub enum GameError {
@@ -65,26 +79,31 @@ impl Game {
     pub fn word_len(&self) -> usize {
         self.keyword.len()
     }
+
+    fn process_blocks(&self, guess: &str) -> Vec<PositionInfo> {
+        guess.chars().enumerate()// map each character to the PositionInfo enum
+            .map(|(i,c)| {
+                let kwc: Vec<_> = self.keyword.chars().collect();
+                match c {
+                    _ if c == kwc[i] => PositionInfo::Correct(c), // correct character, placement
+                    _ if kwc.contains(&c) => PositionInfo::Placement(c), // correct character, incorrect placement
+                    _ => PositionInfo::Incorrect(c), // incorrect character
+                }
+            }).collect()
+    }
     fn process_guess(&mut self, guess: &str) -> GameResult {
         self.guesses += 1;
+        let pos = self.process_blocks(guess);
         match guess {
             _ if guess == self.keyword => {
-                GameResult::Win(self.guesses)
+                GameResult::Win(self.guesses, pos)
             }
             _ => {
                 if self.guesses >= self.limit {
-                    GameResult::Lose // Lost the game (if this wasnt right you don't get any more guesses...)
+                    GameResult::Lose(pos) // Lost the game (if this wasnt right you don't get any more guesses...)
                 } else {    // Process the guess
                     GameResult::Guess(
-                        guess.chars().enumerate()// map each character to the PositionInfo enum
-                            .map(|(i,c)| {
-                                let kwc: Vec<_> = self.keyword.chars().collect();
-                                match c {
-                                    _ if c == kwc[i] => PositionInfo::Correct(c), // correct character, placement
-                                    _ if kwc.contains(&c) => PositionInfo::Placement(c), // correct character, incorrect placement
-                                    _ => PositionInfo::Incorrect(c), // incorrect character
-                                }
-                            }).collect()
+                        pos
                     )
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod ui;
 use crate::Wordle::{Game, GameResult, GameError};
 
 fn main() -> io::Result<()> {
-    let mut game = Game::new("foo", 6);
+    let mut game = Game::new("helloworld", 6);
     loop {
         let mut buffer = String::new();
         let mut stdin = io::stdin(); // We get `Stdin` here.
@@ -13,13 +13,13 @@ fn main() -> io::Result<()> {
         match game.guess(buffer.trim()) {
             Ok(g) => {
                 match g {
-                    GameResult::Win(_) => {println!("{}",g);break},
-                    GameResult::Lose => {println!("{}",g);break;},
+                    GameResult::Win(_, _) => {println!("{}",g);break},
+                    GameResult::Lose(_) => {println!("{}",g);break;},
                     _ => println!("{}",g)
                 }},
             Err(e) => {
                 match e {
-                    GameError::BadLen(_) => println!("Too short/long guess! A wordle word is 5 letters."),
+                    GameError::BadLen(_) => println!("Too short/long guess! Word length is {} letters.", game.word_len()),
                     _ => {dbg!(e);break;}
                 }
             },
